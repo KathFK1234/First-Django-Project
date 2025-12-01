5fb87806e52b7ba186490f7d7e40d11a6f430175
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2025-12-01 15:23:41 +0300

    Added class-based view to views.py file

--- firstProject/first_django_app/views.py
+++ firstProject/first_django_app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from django.views import View
 
 # Create your views here.
+# Make sure to link the views to addresses, ie, URL mapping
+# Function-based view:
 def hello_world(request):
     return HttpResponse("Hello World!")
+
+# Class-based view:
+class first_classb_view():
+    def get(self, request):
+        return HttpResponse("Created the first class-based view!")
