c7115d7ae4078378d4c5e1f095cb2c69e3e44e67
Katheu Kilonzo <fkatheukilonzo@gmail.com>
2025-12-02 10:30:52 +0300

    Defined schema for DB in models.py file for second app

--- firstProject/second_django_app/models.py
+++ firstProject/second_django_app/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
 class Reservation(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    reservation_date = models.DateField()
-    reservation_time = models.DateTimeField(auto_now=True)
-    table_reserved = models.IntegerField(default=0)
-    guest_count = models.IntegerField(default=0)
+    name = models.CharField(max_length=255)
+    contact = models.IntegerField(default='0700000000')
+    tables_reserved = models.IntegerField(default='0')
+    guest_count = models.IntegerField(default='0')
+    reservation_date = models.DateField(default=date.today())
+    reservation_time = models.DateField(auto_now=True)
